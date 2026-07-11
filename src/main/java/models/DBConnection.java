75fd64d8195789eb33d8307feca53026c3c15e11
anshul-089 <anshuldhoble576@gmail.com>
2026-07-11 12:56:18 +0530

    Updated database connection for online MySQL deployment

--- src/main/java/models/DBConnection.java
+++ src/main/java/models/DBConnection.java
@@ -2,21 +2,27 @@ package models;
 
 import java.sql.*;
 
-public class DBConnection 
-{	
-	public static Connection getConnection()
-	{
-		Connection con=null;
-		
-		try
-		{
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/SecureBankProject","root","Anshul@1810");
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		return con;
-	}
+public class DBConnection
+{
+    private static final String URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12832759";
+    private static final String USERNAME = "sql12832759";
+    private static final String PASSWORD = "jMBfRlJNIw";
+
+    public static Connection getConnection()
+    {
+        Connection con = null;
+
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
 }

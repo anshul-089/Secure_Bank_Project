da533e02b0c7c9862b47238591c470520be9e12c
anshul-089 <anshuldhoble576@gmail.com>
2026-07-12 14:58:18 +0530

    Fix TiDB Cloud database connection

--- src/main/java/models/DBConnection.java
+++ src/main/java/models/DBConnection.java
@@ -1,11 +1,17 @@
 package models;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBConnection
 {
-    private static final String URL = "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/bankdb?sslMode=VERIFY_IDENTITY&enabledTLSProtocols=TLSv1.2";
-    private static final String USERNAME = "3tLRqcGUxTz5.root";
+    private static final String URL =
+            "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/bankdb?sslMode=VERIFY_IDENTITY";
+
+    // Correct username from your TiDB Cloud Connect page
+    private static final String USERNAME = "3tLRqcGUxTz5abU.root";
+
+    // Your TiDB Cloud password
     private static final String PASSWORD = "c6tC2qaw6SqPCiy2";
 
     public static Connection getConnection()
@@ -15,11 +21,12 @@ public class DBConnection
         try
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
             con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Database Connected Successfully!");
         }
-        catch(Exception e)
+        catch (Exception e)
         {
+            System.out.println("Database Connection Failed!");
             e.printStackTrace();
         }
 

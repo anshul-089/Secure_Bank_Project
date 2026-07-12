62466ca63ada23071255c678fd111bc82e88968b
anshul-089 <anshuldhoble576@gmail.com>
2026-07-12 14:48:58 +0530

    Fix TiDB Cloud database username configuration

--- src/main/java/models/DBConnection.java
+++ src/main/java/models/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DBConnection
 {
     private static final String URL = "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/bankdb?sslMode=VERIFY_IDENTITY&enabledTLSProtocols=TLSv1.2";
-    private static final String USERNAME = "3tLRqcGUxTz5";
+    private static final String USERNAME = "3tLRqcGUxTz5.root";
     private static final String PASSWORD = "c6tC2qaw6SqPCiy2";
 
     public static Connection getConnection()
